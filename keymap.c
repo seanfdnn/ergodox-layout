418bbdfd9f2402bbaf5c6c1314ed3646e9814504
Gergely Nagy <algernon@madhouse-project.org>
2016-05-13 09:02:37 +0200

    EDIT: The thumb key goes back to BASE
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- keymap.c
+++ keymap.c
@@ -348,7 +348,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                                                ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS  ,KC_TRNS ,KC_TRNS
                                                                                  ,KC_TRNS ,KC_W    ,KC_TRNS  ,KC_TRNS ,KC_TRNS
 
-                                                               ,KC_TRNS ,KC_TRNS
+                                                               ,KC_FN1  ,KC_TRNS
                                                                ,KC_TRNS
                                                                ,KC_TRNS ,KC_TRNS  ,KC_TRNS
     ),
